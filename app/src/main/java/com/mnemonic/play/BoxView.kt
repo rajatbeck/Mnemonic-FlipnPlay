5d354ae382932eb9f1a2cbc138afaab955fa3c17
RajatMarcel Beck <rajatmarcelbeck@RajatMarcels-MacBook-Air.local>
2019-05-28 23:27:08 +0530

    converted to lamdas

--- app/src/main/java/com/mnemonic/play/BoxView.kt
+++ app/src/main/java/com/mnemonic/play/BoxView.kt
@@ -24,6 +24,34 @@ class BoxView : View {
     lateinit var row: Number
     lateinit var col: Number
 
+
+    var imageList: List<Int> = arrayListOf(
+        R.drawable.ic_beach,
+        R.drawable.ic_audio,
+        R.drawable.ic_dollar,
+        R.drawable.ic_car,
+        R.drawable.ic_cloud,
+        R.drawable.ic_camera,
+        R.drawable.ic_android,
+        R.drawable.ic_bluetooth,
+        R.drawable.ic_cake,
+        R.drawable.ic_bulb,
+        R.drawable.ic_drink,
+        R.drawable.ic_food,
+        R.drawable.ic_flower,
+        R.drawable.ic_heart,
+        R.drawable.ic_hot,
+        R.drawable.ic_location,
+        R.drawable.ic_laptop,
+        R.drawable.ic_paw,
+        R.drawable.ic_lock,
+        R.drawable.ic_sofa,
+        R.drawable.ic_plane,
+        R.drawable.ic_train,
+        R.drawable.ic_star,
+        R.drawable.ic_world
+    )
+
     companion object {
         val TAG: String = BoxView::class.java.simpleName
     }
@@ -70,7 +98,7 @@ class BoxView : View {
         paint.style = Paint.Style.FILL
 
         strokePaint = Paint()
-        strokePaint.color = Color.WHITE
+        strokePaint.color = Color.DKGRAY
         strokePaint.style = Paint.Style.STROKE
         strokePaint.strokeWidth = 2F
 
@@ -79,17 +107,39 @@ class BoxView : View {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        val rowWidth = width / row.toInt()
+        val colHeight = height / col.toInt()
+        captureRect(rowWidth, colHeight, rectArr, _captureRect)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        if(measuredWidth<measuredHeight) {
+        if (measuredWidth < measuredHeight) {
             setMeasuredDimension(measuredWidth, measuredWidth)
-        }else{
+        } else {
             setMeasuredDimension(measuredHeight, measuredHeight)
         }
 
     }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val rowWidth = width / row.toInt()
+        val colHeight = height / col.toInt()
+        drawRect(rowWidth, colHeight, canvas, _drawRect)
+    }
+
+
+    private fun drawRect(rowWidth: Int, colHeight: Int, canvas: Canvas?, action: (Canvas?, Int, Int) -> Unit) =
+        action(canvas, rowWidth, colHeight)
+
+    private fun captureRect(
+        rowWidth: Int,
+        colHeight: Int,
+        rectArray: Array<Array<Rect>>,
+        action: (Array<Array<Rect>>, Int, Int) -> Unit
+    ) = action(rectArray, rowWidth, colHeight)
+
 
     private fun reconcileSize(contentSize: Int, measureSpec: Int): Int {
         val mode = MeasureSpec.getMode(measureSpec)
@@ -106,23 +156,31 @@ class BoxView : View {
         }
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        val rowWidth = width / row.toInt()
-        val colHeight = height / col.toInt()
-
+    private val _drawRect: (Canvas?, Int, Int) -> Unit = { canvas, rowWidth, colHeight ->
         var x: Float
         var y = 0F
-        for (i in 1..rowWidth) {
+        for (i in 0 until row.toInt()) {
             x = 0F
-            for (j in 1..colHeight) {
+            for (j in 0 until col.toInt()) {
                 canvas?.drawRect(x, y, x + rowWidth.toFloat(), y + colHeight.toFloat(), paint)
                 canvas?.drawRect(x, y, x + rowWidth.toFloat(), y + colHeight.toFloat(), strokePaint)
                 x = x.plus(rowWidth)
             }
             y = y.plus(colHeight)
         }
+    }
 
+    private val _captureRect: (Array<Array<Rect>>, Int, Int) -> Unit = { rectArr, rowWidth, colHeight ->
+        var x: Float
+        var y = 0F
+        for (i in 0 until row.toInt()) {
+            x = 0F
+            for (j in 0 until col.toInt()) {
+                rectArr[i][j] = Rect(x.toInt(), y.toInt(), x.toInt() + rowWidth, y.toInt() + colHeight)
+                x = x.plus(rowWidth)
+            }
+            y = y.plus(colHeight)
+        }
 
     }
 
--- app/src/main/java/com/mnemonic/play/MainActivity.kt
+++ app/src/main/java/com/mnemonic/play/MainActivity.kt
@@ -1,42 +1,12 @@
 package com.mnemonic.play
 
-,
 
-import android.graphics.drawable.Drawable
-
-,
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
 
-    var imageList: List<Int> = arrayListOf(
-        R.drawable.ic_beach,
-        R.drawable.ic_audio,
-        R.drawable.ic_dollar,
-        R.drawable.ic_car,
-        R.drawable.ic_cloud,
-        R.drawable.ic_camera,
-        R.drawable.ic_android,
-        R.drawable.ic_bluetooth,
-        R.drawable.ic_cake,
-        R.drawable.ic_bulb,
-        R.drawable.ic_drink,
-        R.drawable.ic_food,
-        R.drawable.ic_flower,
-        R.drawable.ic_heart,
-        R.drawable.ic_hot,
-        R.drawable.ic_location,
-        R.drawable.ic_laptop,
-        R.drawable.ic_paw,
-        R.drawable.ic_lock,
-        R.drawable.ic_sofa,
-        R.drawable.ic_plane,
-        R.drawable.ic_train,
-        R.drawable.ic_star,
-        R.drawable.ic_world
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
